e77a2ce3a96f81d8a5be3b1b3fc82d7f84366369
Almog <almoglevi1008@gmail.com>
2022-04-08 10:43:13 +0300

    Code fixed according to the comment

--- my_filter.py
+++ my_filter.py
@@ -1,14 +1,11 @@
-def my_function(iterate):
-    if iterate:
-        return True
-
-    return False
-
-
-def my_filter(my_function, my_iterable):
-    return [iterate for iterate in my_iterable if my_function(iterate)]
+def my_filter(my_iterable):
+    """
+    The function receives a list and returns the elements that return true.
+    :param my_iterable: The checked list.
+    :return: Only the object that returns true.
+    """
+    return [iterate for iterate in my_iterable if iterate]
 
 
 if __name__ == "__main__":
-    my_iterable = [1, True, 0, False]
-    print(my_filter(my_function, my_iterable))
+    print(my_filter([1, True, 0, False]))

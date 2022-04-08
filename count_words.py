29a88efc4018f56b818b5e33f1ffbee313556604
Almog <almoglevi1008@gmail.com>
2022-04-08 11:41:34 +0300

    Code fixed according to the comment

--- count_words.py
+++ count_words.py
@@ -3,7 +3,7 @@ def count_words(checked_text):
     The function receives text and returns a dictionary where the values are the words from the text
     and the keys are the length of the word.
     :param checked_text: The text that the function received as a parameter.
-    :return: The text that the function received as a parameter.
+    :return: The dictionary where the values are the words from the text and the keys are the length of the word.
     """
     return {word: len(word) for word in checked_text.split()}
 

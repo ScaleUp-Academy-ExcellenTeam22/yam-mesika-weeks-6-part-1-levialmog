02734b772916be57081f6c6a4e1d0bfd73269c72
Almog <almoglevi1008@gmail.com>
2022-04-08 11:30:39 +0300

    Code fixed according to the comment

--- timer.py
+++ timer.py
@@ -3,9 +3,9 @@ import timeit
 
 def timer(f, *parameters):
     """
-    The function receives a function and some parameters, calls the function f, passes the parameters to it,
+    The function receives a function named f and some parameters, calls function f, passes the parameters to it,
     and returns the run time of function f.
-    :param f: The function which measures the run time.
+    :param f: The function that it's run time is measured.
     :param parameters: The parameters passed to function f.
     :return: Run time of function f.
     """
